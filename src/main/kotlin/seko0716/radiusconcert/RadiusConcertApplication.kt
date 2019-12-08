b53725b0ac2fca7f565afd2bd7539dd0632f6718
Sergey34 <Sergey3440@gmail.com>
2019-12-08 23:05:30 +0400

    Initial commit

---
+++ src/main/kotlin/seko0716/radiusconcert/RadiusConcertApplication.kt
@@ -0,0 +1,11 @@
+package seko0716.radiusconcert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RadiusConcertApplication
+
+fun main(args: Array<String>) {
+    runApplication<RadiusConcertApplication>(*args)
+}

4e607ab6ffabab9f9f53151e6e43c6bd7551b9c7
Sergey34 <Sergey3440@gmail.com>
2019-12-15 20:03:45 +0400

    add template for yandex afisha

---
+++ event/src/main/kotlin/seko0716/radius/concert/event/services/parsers/yandex/afisha/YandexAfishaEventsParser.kt
@@ -0,0 +1,5 @@
+package seko0716.radius.concert.event.services.parsers.yandex.afisha
+
+//https://afisha.yandex.ru/api/events/actual?limit=20&offset=0&hasMixed=0&city=saratov
+class YandexAfishaEventsParser {
+}

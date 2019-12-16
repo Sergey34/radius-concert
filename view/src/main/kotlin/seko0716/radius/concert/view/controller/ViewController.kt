f08e0aff5f78158361b7d784f85c00bfd2d56fe1
Sergey34 <Sergey3440@gmail.com>
2019-12-16 21:57:14 +0400

    add controller

--- view/src/main/kotlin/seko0716/radius/concert/view/controller/ViewController.kt
+++ view/src/main/kotlin/seko0716/radius/concert/view/controller/ViewController.kt
@@ -1,4 +1,14 @@
 package seko0716.radius.concert.view.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
 class ViewController {
+    @RequestMapping("/")
+    fun index(model: Model): String {
+        return "index"
+    }
 }

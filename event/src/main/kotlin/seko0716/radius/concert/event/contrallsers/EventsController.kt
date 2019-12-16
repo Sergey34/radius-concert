64d22c21d9282daa06955e3fcf42f7d7c4a871a4
Sergey34 <Sergey3440@gmail.com>
2019-12-16 20:23:37 +0400

    add rest for event on distance

--- event/src/main/kotlin/seko0716/radius/concert/event/contrallsers/EventsController.kt
+++ event/src/main/kotlin/seko0716/radius/concert/event/contrallsers/EventsController.kt
@@ -2,7 +2,9 @@ package seko0716.radius.concert.event.contrallsers
 
 import kotlinx.coroutines.flow.Flow
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.geo.Metrics
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import seko0716.radius.concert.event.domains.Event
 import seko0716.radius.concert.event.services.EventService
@@ -21,5 +23,14 @@ class EventsController @Autowired constructor(
     @GetMapping("/update")
     suspend fun updateData(): Flow<Event> {
         return parserService.updateData()
+    }
+
+    @GetMapping("/events/{currentCity}/{distance}/{metric}")
+    suspend fun getEvents(
+        @PathVariable currentCity: String,
+        @PathVariable distance: Double,
+        @PathVariable metric: Metrics
+    ): Flow<Event> {
+        return eventService.getEvents(currentCity, distance, metric)
     }
 }
--- event/src/main/kotlin/seko0716/radius/concert/event/services/EventService.kt
+++ event/src/main/kotlin/seko0716/radius/concert/event/services/EventService.kt
@@ -1,16 +1,31 @@
 package seko0716.radius.concert.event.services
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.geo.Distance
+import org.springframework.data.geo.Metrics
 import org.springframework.stereotype.Service
+import seko0716.radius.concert.event.config.isNan
 import seko0716.radius.concert.event.domains.Event
 import seko0716.radius.concert.event.repository.EventRepository
 
 @Service
 class EventService @Autowired constructor(
-    private val eventRepository: EventRepository
+    private val eventRepository: EventRepository,
+    private val geocodeService: GeocodeService
 ) {
     suspend fun getAllEvents(): Flow<Event> {
         return eventRepository.getAllEvents()
+    }
+
+    suspend fun getEvents(city: String, distance: Double, metric: Metrics): Flow<Event> {
+        return geocodeService.getGeocode(city).run {
+            if (!isNan()) {
+                eventRepository.getEvents(this, Distance(distance, metric))
+            } else {
+                emptyFlow()
+            }
+        }
     }
 }

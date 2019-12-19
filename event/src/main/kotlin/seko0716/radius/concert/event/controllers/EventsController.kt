f5278866e8a67e7bca59596c90cc9b2cc3d0a626
Sergey34 <Sergey3440@gmail.com>
2019-12-19 23:26:14 +0400

    add index htms

--- event/src/main/kotlin/seko0716/radius/concert/event/controllers/EventsController.kt
+++ event/src/main/kotlin/seko0716/radius/concert/event/controllers/EventsController.kt
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.geo.Metrics
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import seko0716.radius.concert.event.domains.Event
 import seko0716.radius.concert.event.services.EventService
 import seko0716.radius.concert.event.services.ParserService
 
 @RestController
+@RequestMapping("/api")
 class EventsController @Autowired constructor(
     private val eventService: EventService,
     private val parserService: ParserService
--- view/src/main/kotlin/seko0716/radius/concert/view/controller/ViewController.kt
+++ view/src/main/kotlin/seko0716/radius/concert/view/controller/ViewController.kt
@@ -2,13 +2,19 @@ package seko0716.radius.concert.view.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 
 @Controller
 class ViewController {
     @RequestMapping("/")
-    fun index(model: Model): String {
+    fun root(model: Model): String {
         return "index"
+    }
+
+    @GetMapping("/events/{currentCity}/{distance}/{metric}")
+    fun events(model: Model): String {
+        return "events"
     }
 }

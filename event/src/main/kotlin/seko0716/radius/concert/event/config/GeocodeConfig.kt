dcae7ba5f30ff77293b3f604e8da584b9c36218d
Sergey34 <Sergey3440@gmail.com>
2019-12-29 00:16:57 +0400

    add persistent db

--- event/src/main/kotlin/seko0716/radius/concert/event/config/GeocodeConfig.kt
+++ event/src/main/kotlin/seko0716/radius/concert/event/config/GeocodeConfig.kt
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.geo.Point
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.count
 import seko0716.radius.concert.event.domains.Geocode
 import seko0716.radius.concert.event.services.GeocodeService
 import java.io.File
 import javax.annotation.PostConstruct
 
-
+@Profile("load_geocode_config")
 @Configuration
 class GeocodeConfig @Autowired constructor(
     private val mongoTemplate: MongoTemplate,
@@ -24,6 +26,9 @@ class GeocodeConfig @Autowired constructor(
 
     @PostConstruct
     fun importGeocodes() {
+        if (mongoTemplate.count<Geocode>() != 0L) {
+            return
+        }
         println("load geocodes")
         val distinctBy = mapper.readValue<List<Pair<String, SerderPoint>>>(File("geocodes.json"))
             .map { Geocode(it.first.toLowerCase(), it.first, Point(it.second.y, it.second.x)) }

fc0bad30e20468fa5db62fc5c9e2abbbf2769ac1
Sergey34 <Sergey3440@gmail.com>
2020-01-18 11:58:48 +0400

    add redirect to single event page for events

--- view/static/js/fillEvents.js
+++ view/static/js/fillEvents.js
@@ -3,13 +3,13 @@ let events_stored = [];
 const template = "{{#.}}\n" +
     "<div class=\"col-lg-6\">\n" +
     "    <div class=\"room-wrap d-md-flex ftco-animate fadeInUp\">\n" +
-    "        <a class=\"img  {{#order_md_last}}order-md-last{{/order_md_last}}\" href=\"{{event.url}}\" style=\"background-image: url({{event.image.microdata.url}});\"></a>\n" +
+    "        <a class=\"img  {{#order_md_last}}order-md-last{{/order_md_last}}\" href=\"/event/{{id}}\" style=\"background-image: url({{event.image.microdata.url}});\"></a>\n" +
     "        <div class=\"half {{class}} d-flex align-items-center\">\n" +
     "            <div class=\"text p-4 text-center\">\n" +
     "                <p class=\"mb-0\">{{#event.tickets}}<span class=\"price mr-1\">{{price.min}}-{{price.max}}р</span>{{/event.tickets}}" +
     "                <p class=\"mb-0\"><span class=\"price mr-1\">{{city.name}}, {{scheduleInfo.dateStarted}}-{{scheduleInfo.dateEnd}}</span>" +
-    "                <h3 class=\"mb-3\"><a href=\"{{event.url}}\">{{event.title}}</a></h3>\n" +
-    "                <p class=\"pt-1\"><a class=\"btn-custom px-3 py-2 rounded\" href=\"{{event.url}}\">Подробнее ({{city.type}})\n" +
+    "                <h3 class=\"mb-3\"><a href=\"/event/{{id}}\">{{event.title}}</a></h3>\n" +
+    "                <p class=\"pt-1\"><a class=\"btn-custom px-3 py-2 rounded\" href=\"/event/{{id}}\">Подробнее ({{city.type}})\n" +
     "                    <span class=\"icon-long-arrow-right\"></span></a></p>\n" +
     "            </div>\n" +
     "        </div>\n" +

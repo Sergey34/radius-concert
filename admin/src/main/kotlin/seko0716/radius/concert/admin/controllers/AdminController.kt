fef2bfb57887f3cb77eb557290074021ad4594b8
Sergey34 <Sergey3440@gmail.com>
2020-01-25 18:44:11 +0400

    add update status page

--- admin/src/main/kotlin/seko0716/radius/concert/admin/controllers/AdminController.kt
+++ admin/src/main/kotlin/seko0716/radius/concert/admin/controllers/AdminController.kt
@@ -1,11 +1,11 @@
 package seko0716.radius.concert.admin.controllers
 
-import kotlinx.coroutines.FlowPreview
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import seko0716.radius.concert.admin.services.ParserService
+import seko0716.radius.concert.admin.services.StatusUpdate
 import seko0716.radius.concert.event.domains.Event
 
 @RestController
@@ -13,9 +13,14 @@ import seko0716.radius.concert.event.domains.Event
 class AdminController @Autowired constructor(
     private val parserService: ParserService
 ) {
-    @FlowPreview
+
     @GetMapping("/update")
     suspend fun updateData(): List<Event> {
         return parserService.updateData()
+    }
+
+    @GetMapping("/update/status")
+    suspend fun status(): StatusUpdate? {
+        return parserService.status()
     }
 }
--- admin/src/main/kotlin/seko0716/radius/concert/admin/services/ParserService.kt
+++ admin/src/main/kotlin/seko0716/radius/concert/admin/services/ParserService.kt
@@ -10,6 +10,8 @@ import seko0716.radius.concert.admin.services.parsers.CityParser
 import seko0716.radius.concert.admin.services.parsers.EventParser
 import seko0716.radius.concert.event.domains.Event
 import seko0716.radius.concert.event.repository.EventRepository
+import java.time.Clock
+import java.time.ZonedDateTime
 
 @Service
 class ParserService @Autowired constructor(
@@ -17,19 +19,30 @@ class ParserService @Autowired constructor(
     val eventParser: EventParser,
     val eventRepository: EventRepository
 ) {
+    var statusUpdate: StatusUpdate? = null
+
 
     suspend fun updateData(): List<Event> = coroutineScope {
+        statusUpdate = StatusUpdate(ZonedDateTime.now(Clock.systemUTC()))
         val events = cityParser.parse()
             .map {
                 async {
                     eventRepository.saveAllSync(eventParser.parse(it))
                 }
             }.flatMap { it.await() }
+        statusUpdate?.apply {
+            end = ZonedDateTime.now(Clock.systemUTC())
+            count = events.size
+        }
         events
     }
 
     @Scheduled(cron = "\${spring.application.events.dataset.refresh}")
     fun updateScheduleData() = runBlocking {
         val updateData = updateData()
+    }
+
+    fun status(): StatusUpdate? {
+        return statusUpdate
     }
 }

62e434c702896f9f9ecae2ae7076f0ee3557f8de
Sergey34 <Sergey3440@gmail.com>
2020-01-26 23:24:26 +0400

    refactoring

--- admin/src/main/kotlin/seko0716/radius/concert/admin/config/GeocodeConfig.kt
+++ admin/src/main/kotlin/seko0716/radius/concert/admin/config/GeocodeConfig.kt
@@ -7,12 +7,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.data.geo.Point
 import seko0716.radius.concert.geocode.domains.Geocode
 import java.io.File
 
-@Profile("load_geocode_config")
 @Configuration
 class GeocodeConfig
 @Autowired constructor(
